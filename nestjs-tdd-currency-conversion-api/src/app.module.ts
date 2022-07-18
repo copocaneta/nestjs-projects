5b9acaf59406f02567416ef199d33248a6f9b416
Thiago Bernardi <copocaneta@gmail.com>
2022-07-18 14:57:22 -0300

    chore: created service and module for Exchange

--- nestjs-tdd-currency-conversion-api/src/app.module.ts
+++ nestjs-tdd-currency-conversion-api/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ExchangeModule } from './exchange/exchange.module';
 
 @Module({
-  imports: [],
+  imports: [ExchangeModule],
   providers: [],
 })
 export class AppModule {}
