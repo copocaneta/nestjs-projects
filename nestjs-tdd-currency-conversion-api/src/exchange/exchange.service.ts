8ecacd45b823893c1c9bbd7124495fe99bc3454d
Thiago Bernardi <copocaneta@gmail.com>
2022-07-18 23:14:13 -0300

    feat: ensure convertAmount returns the conversion value

--- nestjs-tdd-currency-conversion-api/src/exchange/exchange.service.ts
+++ nestjs-tdd-currency-conversion-api/src/exchange/exchange.service.ts
@@ -7,13 +7,14 @@ export class CurrenciesService {
 @Injectable()
 export class ExchangeService {
   constructor(private currenciesService: CurrenciesService) {}
-  async convertAmount({ from, to, amount }): Promise<void> {
+  async convertAmount({ from, to, amount }): Promise<any> {
     if (!from || !to || !amount) {
       throw new BadRequestException();
     }
     try {
       const currencyFrom = await this.currenciesService.getCurrency(from);
       const currencyTo = await this.currenciesService.getCurrency(to);
+      return { amount: (currencyFrom.value / currencyTo.value) * amount };
     } catch (error) {
       throw new Error(error);
     }
